aabc5409682a5f59804326167bd4ae73e696c5d5
jamesfe <james.ferrara@gmail.com>
2017-02-01 21:48:30 +0100

    Versioning, add authors

--- src/donald_comment_stats.py
+++ src/donald_comment_stats.py
@@ -9,6 +9,8 @@ import pickle
 import os
 import argparse
 
+VERSION = '1.2.0'
+
 
 def generate_pickle_file(target_file, pickle_file_name):
     """Generate a pickle file and return some data about that the_donald's scores."""
@@ -24,7 +26,13 @@ def generate_pickle_file(target_file, pickle_file_name):
                 except Exception as e:
                     pass
                     print e
-                year[day].append((item.get('score', 0), item.get('ups', 0), item.get('controversiality', 0)))
+                day_dict = {
+                    'score': item.get('score', 0),
+                    'ups': item.get('ups', 0),
+                    'controversiality': item.get('controversiality', 0),
+                    'author': item.get('author', '')
+                }
+                year[day].append(day_dict)
     with file(pickle_file_name, 'w') as outfile:
         pickle.dump(year, outfile)
     return year
@@ -37,7 +45,7 @@ if __name__ == '__main__':
 
     target_dump_file = args.file
     target_base = os.path.basename(target_dump_file)
-    target_pickle_file = '../pickles/{}.pickle'.format(target_base)
+    target_pickle_file = '../pickles/{}.{}.pickle'.format(target_base, VERSION)
 
     if not os.path.isfile(target_pickle_file):
         year = generate_pickle_file(target_dump_file, target_pickle_file)
@@ -50,10 +58,12 @@ if __name__ == '__main__':
         num_comments = len(value)
         if num_comments > 0:
             json_obj[key] = {
-                'avg_score': reduce(lambda x, y: x + y[0], value, 0) / num_comments,
-                'avg_ups': reduce(lambda x, y: x + y[1], value, 0) / num_comments,
-                'avg_contro': reduce(lambda x, y: x + y[2], value, 0) / num_comments
+                'avg_score': reduce(lambda x, y: x + y['score'], value, 0) / num_comments,
+                'avg_ups': reduce(lambda x, y: x + y['ups'], value, 0) / num_comments,
+                'avg_contro': reduce(lambda x, y: x + y['controversiality'], value, 0) / num_comments,
+                'total_comments': num_comments,
+                'num_authors': len(set([_['author'] for _ in value]))
             }
 
-    with file('../output/{}.synopsis.json'.format(target_base), 'w') as outfile:
+    with file('../output/{}.{}.synopsis.json'.format(target_base, VERSION), 'w') as outfile:
         json.dump(json_obj, outfile, indent=3)
